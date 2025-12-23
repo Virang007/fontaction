b46de779a619d456a001da38c56747b103676aea
Virang007 <acharyavirang96@gmail.com>
2025-12-23 13:01:06 +0530

    fix welcome CICD

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="app-container">
       <div className="card">
-        <h1>{import.meta.env.VITE_APP_TITLE || 'Welcome nexback'}</h1>
+        <h1>{import.meta.env.VITE_APP_TITLE || 'Welcome nexback devops'}</h1>
         <p className="subtitle">Please enter your name below</p>
 
         <form onSubmit={handleSubmit} className="input-group">
